139dcca4c341d4d462cbd967af293ff5f4d592ee
Hale Deng <haledeng88@gmail.com>
2019-01-27 22:49:30 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import './assets/styles/index.styl'
+import VueLazyLoad from 'vue-lazyload'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/images/default.png')
+})
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: (h: any) => h(App)
+}).$mount('#app')
