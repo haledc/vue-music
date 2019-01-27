02802f23bd351ce9970b5d65bb7ecb506bdf54ca
Hale Deng <haledeng88@gmail.com>
2019-01-27 22:48:54 +0800

    update router

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Recommend from '@/views/Recommend.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   mode: 'history',
@@ -10,16 +10,29 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      redirect: '/recommend'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/recommend',
+      name: 'recommend',
+      component: Recommend
     },
-  ],
-});
+    {
+      path: '/singer',
+      name: 'singer',
+      component: () =>
+        import(/* webpackChunkName: "singer" */ './views/Singer.vue')
+    },
+    {
+      path: '/rank',
+      name: 'rank',
+      component: () => import(/* webpackChunkName: "rank" */ './views/Rank.vue')
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: () =>
+        import(/* webpackChunkName: "search" */ './views/Search.vue')
+    }
+  ]
+})
