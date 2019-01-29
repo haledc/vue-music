92547df8c75bd361d427d5ab296b31bd98674313
Hale Deng <haledeng88@gmail.com>
2019-01-29 15:22:04 +0800

    add params interface

--- src/assets/utils/singer.ts
+++ src/assets/utils/singer.ts
@@ -1,10 +1,69 @@
+interface SingerParams {
+  id: string
+  name: string
+}
+
 export default class Singer {
   public id: string
   public name: string
   public avatar: string
-  constructor({ id, name }: any) {
+  constructor({ id, name }: SingerParams) {
     this.id = id
     this.name = name
     this.avatar = `http://y.gtimg.cn/music/photo_new/T001R300x300M000${id}.jpg?max_age=2592000`
   }
+}
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
+export const normalizeSinger = (list: any[]) => {
+  const map: any = {
+    hot: {
+      title: HOT_NAME,
+      items: []
+    }
+  }
+
+  list.forEach((item, index) => {
+    if (index < HOT_SINGER_LEN) {
+      map.hot.items.push(
+        new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        })
+      )
+    }
+
+    const key = item.Findex
+
+    if (!map[key]) {
+      map[key] = {
+        title: key,
+        items: []
+      }
+    }
+
+    map[key].items.push(
+      new Singer({
+        id: item.Fsinger_mid,
+        name: item.Fsinger_name
+      })
+    )
+  })
+
+  const hot = []
+  const ret = []
+
+  for (const key of Object.keys(map)) {
+    const val = map[key]
+    if (val.title.match(/[a-zA-Z]/)) {
+      ret.push(val)
+    } else if (val.title === HOT_NAME) {
+      hot.push(val)
+    }
+  }
+
+  ret.sort((a, b) => a.title.charCodeAt(0) - b.title.charCodeAt(0))
+  return hot.concat(ret)
 }
