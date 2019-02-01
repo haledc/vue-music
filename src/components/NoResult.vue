35a4e58784088e93c7b9e082dfae290fc00d8e3c
Hale Deng <haledeng88@gmail.com>
2019-02-01 23:29:19 +0800

    add NoResult component

---
+++ src/components/NoResult.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="no-result">
+    <div class="no-result-icon"></div>
+    <p class="no-result-text">
+      {{ title }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class NoResult extends Vue {
+  @Prop({ default: '' }) public title!: string
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/variable.styl'
+@import '~@/assets/styles/mixin.styl'
+
+.no-result
+  text-align: center
+
+  .no-result-icon
+    width: 86px
+    height: 90px
+    margin: 0 auto
+    bg-image('no-result')
+    background-size: 86px 90px
+
+  .no-result-text
+    margin-top: 30px
+    font-size: $font-size-medium
+    color: $color-text-d
+</style>
