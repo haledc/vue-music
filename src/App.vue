bb5ca96d4191eefbe4257b009939b3a6e1812648
Hale Deng <haledeng88@gmail.com>
2019-02-01 23:30:19 +0800

    add player view

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <keep-alive>
       <router-view />
     </keep-alive>
+    <Player />
   </div>
 </template>
 
@@ -12,11 +13,13 @@
 import { Vue, Component } from 'vue-property-decorator'
 import Header from '@/components/Header.vue'
 import Tab from '@/components/Tab.vue'
+import Player from '@/views/Player.vue'
 
 @Component({
   components: {
     Header,
-    Tab
+    Tab,
+    Player
   }
 })
 export default class App extends Vue {}
