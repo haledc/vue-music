877d1288b5bce691facadcc748c7801fffb299f0
Hale Deng <haledeng88@gmail.com>
2019-02-03 00:55:49 +0800

    add ProgressCircle component

---
+++ src/components/ProgressCircle.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="progress-circle">
+    <svg
+      :width="radius"
+      :height="radius"
+      viewBox="0 0 100 100"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        class="progress-background"
+        r="50"
+        cx="50"
+        cy="50"
+        fill="transparent"
+      ></circle>
+      <circle
+        r="50"
+        cx="50"
+        cy="50"
+        fill="transparent"
+        :stroke-dasharray="dashArray"
+        :stroke-dashoffset="dashOffset"
+      ></circle>
+    </svg>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class ProgressCircle extends Vue {
+  @Prop({ default: 100 }) public radius!: number
+  @Prop({ default: 0 }) public percent!: number
+
+  public dashArray: number = Math.PI * 100
+
+  get dashOffset() {
+    return (1 - this.percent) * this.dashArray
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/variable.styl'
+
+.progress-circle
+  position: relative
+
+  circle
+    stroke-width: 8px
+    transform-origin: center
+
+    &.progress-background
+      transform: scale(0.9)
+      stroke: $color-theme-d
+
+    &.progress-bar
+      transform: scale(0.9) rotate(-90deg)
+      stroke: $color-theme
+</style>
