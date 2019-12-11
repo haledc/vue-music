80efabd092455e80579f644430dc22e42b5383de
Hale Deng <haledeng88@gmail.com>
2019-12-11 10:20:23 +0800

    refactor: fix slider interface

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,44 @@ const bodyParser = require('body-parser')
 module.exports = {
   devServer: {
     before(app) {
+      app.get('/api/getSliderList', (req, res) => {
+        const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+        const jumpPrefix = 'https://y.qq.com/n/yqq/album/'
+
+        axios
+          .get(url, {
+            headers: {
+              referer: 'https://u.y.qq.com/',
+              host: 'u.y.qq.com'
+            },
+            params: req.query
+          })
+          .then(response => {
+            response = response.data
+            if (response.code === 0) {
+              const slider = []
+              const content = response.focus.data && response.focus.data.content
+              if (content && content.length) {
+                for (let i = 0; i < content.length; i++) {
+                  const item = content[i]
+                  const sliderItem = {}
+                  sliderItem.id = item.id
+                  sliderItem.linkUrl = jumpPrefix + item.jump_info.url + '.html'
+                  sliderItem.picUrl = item.pic_info.url
+                  slider.push(sliderItem)
+                }
+              }
+              res.json({
+                code: 0,
+                data: { slider }
+              })
+            } else {
+              res.json(response)
+            }
+          })
+          .catch(err => console.log(err))
+      })
+
       app.get('/api/getDiscList', (req, res) => {
         const url =
           'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
